5c4dcb805e5ba9bdecfa87e45aea65231fab66dc
Andrew McIndoe <amcindoe@powerhrg.com>
2021-05-26 14:51:55 -0400

    Reduce method

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5]
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((accumulator, element) => {
+    return accumulator + element;
+  }, 0);
+  
+  const product = array.reduce((accumulator, element) => {
+    return accumulator * element;
+  }, 1);
+
+  
+//var monthsById = months.reduce((byId, month) => byId[month.Id] = month, {}); 
+//var months = [ { Id: 1 }, { Id: 2 }, { Id: 3 } ];
+
+
+//const doubledAndSummed = [1, 2, 3].reduce(function(accumulator, element){ 
+  //  return element * 2 + accumulator})
+
+//const reducer = (accumulator, currentValue) 
+// => console.log(array1.reduce(reducer));
